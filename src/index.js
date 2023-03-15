d3fcdd3eb8e706c82e7f85a958f3663bfc5008c2
Manuel Kaufmann <humitos@gmail.com>
2023-03-16 00:04:38 +0100

    remove the initial trigger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-import { setup, compare } from "./extension";
-
-/**
- * Automatically set this up if it is included via `<script>`
- *
- * This actually isn't super reliable, and even Jest misfires this code. So the
- * modules are split up a bit to avoid this.
- */
-if (require.main !== module) {
-  setup();
-}
-
+import { compare } from "./extension";
 export { compare };
